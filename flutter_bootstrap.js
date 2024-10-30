f715d3c22ab03f161c4c5ea9061c52693aaa49aa
João Natividade <87316339+joaosnet@users.noreply.github.com>
2024-10-30 19:07:04 -0300

    corrigindo erros e adicionando responsividade no tela de login

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1039621800"
+    serviceWorkerVersion: "2468652294"
   }
 });
