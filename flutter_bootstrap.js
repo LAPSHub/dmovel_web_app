5bc82afafd813bbc485f260a61eaa8478a9e806c
João Natividade <87316339+joaosnet@users.noreply.github.com>
2025-10-16 11:00:11 -0300

    Add French translations and assets
    Added French language support with fr.json and fr_FR.png. Updated asset manifests and included new FontAwesome font files for improved icon support.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1515643256"
+    serviceWorkerVersion: "1221310965"
   }
 });
